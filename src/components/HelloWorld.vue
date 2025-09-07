e676ed9ef7fe006c56d12c6671e35c346ac09ca8
赵剑锋 <zjf770126604@qq.com>
2025-09-07 16:46:55 +0800

    微信拦截修改

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,15 +1,26 @@
 <script setup>
-import { ref } from 'vue'
-
+import {ref} from 'vue'
+import axios from "axios"
 defineProps({
   msg: String,
 })
 
+const apiUrl = import.meta.env.VITE_APP_SERVICE_URL;
+console.log(apiUrl);
+
+axios.get(apiUrl + '/admin-api/test')
+    .then(response => {
+      console.log(response.data)
+    })
+    .catch(error => {
+      console.error('There was an error!', error)
+    });
 const count = ref(0)
 </script>
 
 <template>
   <h1>你好，千与！</h1>
+  {{apiUrl}}
 </template>
 
 <style scoped>
--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,17 @@
-import { defineConfig } from 'vite'
-import vue from '@vitejs/plugin-vue'
+// vite.config.js
+import { defineConfig } from 'vite';
+import vue from '@vitejs/plugin-vue';
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [vue()],
-})
+  server: {
+    proxy: {
+      // 将 /api/ 开头的请求代理到 http://localhost:4000
+      '/admin-api': {
+        target: 'http://localhost:8080',
+        changeOrigin: true,
+        rewrite: (path) => path.replace(/^\/api/, '')
+      }
+    }
+  }
+});
